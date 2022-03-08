817d6dcc0a59b7eddb7dcd79b46918f594db9317
sireax <sireax@yandex.ru>
2022-03-08 23:45:01 +0400

    read from multiple nodes (not tested)

--- rhosus/registry/util.go
+++ rhosus/registry/util.go
@@ -3,17 +3,8 @@ package registry
 import (
 	control_pb "github.com/parasource/rhosus/rhosus/pb/control"
 	"github.com/parasource/rhosus/rhosus/pb/fs_pb"
-	transport_pb "github.com/parasource/rhosus/rhosus/pb/transport"
 	"sort"
 )
-
-func blocksInfoToPlacement(blocks []*control_pb.BlockInfo) []*transport_pb.BlockPlacementInfo {
-	placement := make([]*transport_pb.BlockPlacementInfo, len(blocks))
-	//for i, block := range blocks {
-	//	placement[i] = &transport_pb.BlockPlacementInfo{BlockID: block.Id, PartitionID: block.PartitionID}
-	//}
-	return placement
-}
 
 func fillAndSortBlocks(blocks []*control_pb.BlockInfo, dBlocks []*fs_pb.Block) []*fs_pb.Block {
 	bMap := make(map[string]uint64, len(blocks))
